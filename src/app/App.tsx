9a1552b4c4440ad21a3eb06be40c5dc1f8e36bb8
alngo <alngo@student.42.fr>
2020-03-09 14:09:04 +0100

    feat(homepage): build homepage.

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,23 @@
-import React from 'react';
-import styled from "styled-components"
+import React from "react";
+import Container from "@material-ui/core/Container";
+import Grid from "@material-ui/core/Grid";
+import Box from "@material-ui/core/Box";
+import Typography from "@material-ui/core/Typography";
 
 function App() {
   return (
-	<div>
-		Coucou
-	</div>
+    <Container >
+      <Grid direction="column" alignItems="center">
+        <Box height="100%">
+          <Typography variant="h3" color="primary">
+            <strong>Seek.</strong>
+          </Typography>
+          <Typography variant="h2" color="secondary">
+            <strong>KNOWLEDGE</strong>
+          </Typography>
+        </Box>
+      </Grid>
+    </Container>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from 'app/App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { ThemeProvider } from "@material-ui/core/styles";
+import theme from "styles/theme";
+import App from "app/App";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <ThemeProvider theme={theme}>
+    <CssBaseline />
+    <App />
+  </ThemeProvider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
